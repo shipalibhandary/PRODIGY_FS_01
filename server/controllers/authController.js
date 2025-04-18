03f8b1da1fbfbae6247b1f0c6f845867622e5836
shipalibhandary <shipalibhandary@gmail.com>
2025-04-18 21:33:30 +0530

    .

---
+++ server/controllers/authController.js
@@ -0,0 +1,88 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import usermodel from '../models/userModel.js';
+
+export const register =async (req,res)=>{
+    const {name,email,password}=req.body;
+
+    if(!name || !email || !password){
+        return res.json({success:false,message:'missing details'});
+    }
+    try{
+        const existinguser= await usermodel.findOne({email});
+        if(existinguser){
+            return res.json({success:false,message:"user already exists"});
+        }
+        const hashedpassword = await bcrypt.hash(password,10);
+        const user=new usermodel({name,email,password:hashedpassword});
+        await user.save();
+
+        const token=jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token',token ,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV ==='production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none':'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({success:true});
+
+
+    }catch(error){
+        res.json({success:false,message:error.message});
+    }
+}
+
+
+
+export const login = async (req,res)=> {
+    const{email, password}= req.body;
+
+    if(!email || !password){
+        return res.json({success:false, message: "email and password are required"});
+    }
+    try{
+        const user = await usermodel.findOne({email});
+
+        if(!user){
+            return res.json({success:false,message:"Invalid Email"});
+        }
+
+        const Ismatch = await bcrypt.compare(password,user.password);
+
+        if(!Ismatch){
+            return res.json({success:false,message:"Invalid password"});
+        }
+        const token=jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token',token ,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV ==='production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none':'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({success:true});
+
+
+
+    }catch(error){
+        return res.json({success:false, message: error.message});
+    }
+}
+export const logout = async(req,res)=> {
+    try{
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure: process.env.NODE_ENV ==='production',
+            sameSite: process.env.NODE_ENV ==='production' ? 'none':'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({success:true, message :"logged out"});
+
+    }catch(error){
+        return res.json({success:false, message: error.message});
+    }
+}
--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";   
 import 'dotenv/config';
+import connectdb from "./config/mongodb.js";
+import authRouter from './routes/authRoutes.js';
 
 const app= express();
 const port = process.env.PORT || 3000;
+
+connectdb
 
 app.use(express.json());
 app.use(cookieParser());
@@ -13,6 +17,8 @@ app.use(cors({credentials:true}));
 app.get('/', (req, res) => {
     res.send("API is working perfectly");
   });
+
+app.use('/api/auth',authRouter);
 app.listen(port,()=> console.log(`server started on port:${port}`));
 
-//this is my checking comment ...please no worry!!
+//this is my checking comment ...please no worry!!!S
